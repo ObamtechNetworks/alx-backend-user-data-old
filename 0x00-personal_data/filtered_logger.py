49dce779545814a8b9be01040b07a790f7448eb3
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-05 01:41:38 +0100

    class filtering

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -22,3 +23,23 @@ def filter_datum(fields: List[str],
     return re.sub(pattern,
                   lambda match: match.group().split('=')[0] + '=' + redaction,
                   message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initialize the formatter with the specified fields to redact """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in incoming log records using filter_datum """
+        original_message = super().format(record)
+        return filter_datum(self.fields,
+                            self.REDACTION, original_message,
+                            self.SEPARATOR)
